bd9f8e4ec836b16a43277d40fce3397ab63c8873
Antonio Vivas <antoniovivascavalcante@gmail.com>
2022-05-24 10:43:27 +0200

    Add task, modal show and hide

--- js/taskProject/main.js
+++ js/taskProject/main.js
@@ -4,3 +4,26 @@ $("#search-task").on("keyup", function(){
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     })
 })
+$('#new-task').on('click', function(){
+    $('#exampleModal').modal('hide');
+    $('#exampleModal2').modal('show');
+})
+//New task create
+$('#task-form').submit(function(){
+    $('#exampleModal').modal('show');
+    $('#exampleModal2').modal('hide');
+    var taskName = $('#task-name').val();
+    var deadline = $('#deadline').val();
+    var index = $('#task-tbody tr').length + 1;
+    $('#task-tbody').append(
+        "<tr>"+
+            "<th>"+index+"</th>"+
+            "<td >"+taskName+"</td>"+
+            "<td>"+deadline+"</td>"+
+            "<td><button class='btn btn-success complete-btn'>Complete</button><button class='btn btn-danger delete-btn'>Delete</button><button class='btn btn-info' style='color: white;'>Edit</button></td>"+
+        "</tr>"
+    );
+    $('#task-form input').each(function(){
+        $(this).val('');
+    })
+})
