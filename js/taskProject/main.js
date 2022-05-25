915e057f8783a06fd9eb34ccdd85640bf3ae2505
Antonio Vivas <antoniovivascavalcante@gmail.com>
2022-05-25 22:02:53 +0200

    swap item, index updating on swap, creating and removing

--- js/taskProject/main.js
+++ js/taskProject/main.js
@@ -2,6 +2,10 @@ var inputTaskName = $('#task-name');
 var inputDeadline = $('#deadline');
 var modalTasks = $('#exampleModal');
 var modalCreateTask = $('#exampleModal2');
+var tBody = $('#task-tbody');
+var tRow = $('#task-tbody tr');
+var taskOnDrag;
+var taskUnder;
 $("#search-task").on("keyup", function(){
     var value = $(this).val().toLowerCase();
     $("#task-tbody tr").each(function(){
@@ -20,6 +24,7 @@ $('#new-task').on('click', function(){
 })
 //Form submit (new task or edit task)
 $('#task-form').submit(function(){
+    tRow = $('#task-tbody tr');
     modalTasks.modal('show');
     modalCreateTask.modal('hide');
     index = inputTaskName.attr('data-index');
@@ -27,18 +32,18 @@ $('#task-form').submit(function(){
     deadline = inputDeadline.val();
     if(index > 0){
         //Edit
-        var taskRow = $('#task-tbody').children().eq(index - 1);
-        taskRow.children().eq(1).text(taskName);
-        taskRow.children().eq(2).text(deadline);
+        tRow = tBody.children().eq(index - 1);
+        tRow.children().eq(1).text(taskName);
+        tRow.children().eq(2).text(deadline);
     }else{
         //New task
-        index = $('#task-tbody tr').length + 1;
-        $('#task-tbody').append(
-            "<tr>"+
+        index = tRow.length + 1;
+        tBody.append(
+            "<tr draggable = 'true'>"+
                 "<th>"+index+"</th>"+
                 "<td >"+taskName+"</td>"+
                 "<td>"+deadline+"</td>"+
-                "<td><button class='btn btn-success complete-btn'><img src='img/verifica.png' alt=''></button><button class='btn btn-danger delete-btn'><img src='img/lata-de-lixo.png' alt=''></button><button class='btn btn-info edit-btn' style='color: white;'><img src='img/editar.png' alt=''></button></td>"+
+                "<td><button  draggable = 'false' class='btn btn-success complete-btn'><img src='img/verifica.png' alt=''></button><button draggable = 'false' class='btn btn-danger delete-btn'><img src='img/lata-de-lixo.png' alt=''></button><button draggable = 'false' class='btn btn-info edit-btn' style='color: white;'><img src='img/editar.png' alt=''></button></td>"+
             "</tr>"
         );
         completeTask();
@@ -48,32 +53,37 @@ $('#task-form').submit(function(){
 })
 //Task actions
 function completeTask(){
-    $('.complete-btn').each(function(){
-        $(this).on('click', function(){
-            $(this).parents().eq(1).remove();
-            alert('Nice work!!');
-    
-        })
-    });
+    $('#task-tbody tr:last-child .complete-btn').on('click', function(){
+        $(this).parents().eq(1).remove();
+        alert('Nice work!!');
+        $('#task-tbody tr').each(function (index) {
+            $(this).find('th:first').text(index + 1);
+        });
+    })
+
 }
 function deleteTask(){
-    $('.delete-btn').each(function(){
-        $(this).on('click', function(){
-            $(this).parents().eq(1).remove();
-            alert('Task removed!');
-    
-        })
+    $('#task-tbody tr:last-child .delete-btn').on('click', function(){
+        $(this).parents().eq(1).remove();
+        alert('Task deleted!');
+        $('#task-tbody tr').each(function (index) {
+            $(this).find('th:first').text(index + 1);
+        });
+    })
+    $('#task-tbody tr').each(function (index) {
+        $(this).find('th:first').text(index + 1);
     });
 }
 function editTask(){
+    tRow = $('#task-tbody tr');
     $('.edit-btn').each(function(i){
         $(this).on('click', function(){
             modalTasks.modal('hide');
             modalCreateTask.modal('show');
             $('#exampleModalLabel2').text('Edit task');
-            var index = $('#task-tbody tr').eq(i).children().eq(0).text();
-            var taskName = $('#task-tbody tr').eq(i).children().eq(1).text();
-            var deadline = $('#task-tbody tr').eq(i).children().eq(2).text();
+            var index = tRow.eq(i).children().eq(0).text();
+            var taskName = tRow.eq(i).children().eq(1).text();
+            var deadline = tRow.eq(i).children().eq(2).text();
             inputTaskName.val(taskName);
             inputDeadline.val(deadline);
             inputTaskName.attr('data-index', index);
@@ -83,3 +93,26 @@ function editTask(){
 completeTask();
 deleteTask();
 editTask();
+//Drag and drop
+function dragStart (event){
+    taskOnDrag = $(event.target);
+}
+function dragOver(event){
+    taskUnder = $(event.target.parentNode);
+    event.preventDefault();
+}
+tBody.on('dragstart', dragStart);
+tBody.on('dragover', dragOver);
+tBody.on('drop', function drop (){
+    var index1 = taskOnDrag.children().eq(0);
+    var index2 = taskUnder.children().eq(0);
+    if(index1.text() < index2.text()){
+        $(taskOnDrag).insertAfter(taskUnder);
+    }else{
+        $(taskOnDrag).insertBefore(taskUnder);
+    }
+    $('#task-tbody tr').each(function (index) {
+        $(this).find('th:first').text(index + 1);
+    });
+});
+
